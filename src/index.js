37804d12484171a3bad516e1603df7cbb64d174e
Wade <158378370+wadevenz@users.noreply.github.com>
2024-09-03 20:55:09 +1000

    feat: anync and await

--- src/index.js
+++ src/index.js
@@ -22,28 +22,50 @@ console.log(contentToWrite);
 // Promises based version of nose fs
 const fs = require("node:fs/promises");
 
-console.log("Before the promise");
-fs.writeFile(".env", contentToWrite).then(() => {
-    console.log("After the file has been written");
-}).then(() => {
-    fs.writeFile(".someOtherFile", contentToWrite).then(() => {
-        console.log("After the file has been written in the 2nd block");
 
-        fs.writeFile(".someOtherFile2", contentToWrite).then(() => {
-            console.log("After the file has been written in the 3rd block");
-        }).catch((error) => {
-            console.log("Error occure in a deeply nested promis chain");
-        });
-    }).catch(error => {
-        console.log("Error occured in the 2nd promise chain");
+async function writeEnvFile() {
+    console.log("Before the await");
+    try {
+        let result = await fs.writeFile(".env", contentToWrite);
+        console.log(result);
+    } catch (error) {
+        console.log("Error occured writing the file;", error);
+    }
+    console.log("After the await");
+}
+writeEnvFile();
+
+async function app() {
+    console.log("Before calling the async writer");
+    await writeEnvFile().then(() => {
+        console.log("File writing maybe finished, Who knows!");
     });
+    console.log("After calling the async writer")
+}
+app();
+
+// console.log("Before the promise");
+// fs.writeFile(".env", contentToWrite).then(() => {
+//     console.log("After the file has been written");
+// }).then(() => {
+//     fs.writeFile(".someOtherFile", contentToWrite).then(() => {
+//         console.log("After the file has been written in the 2nd block");
+
+//         fs.writeFile(".someOtherFile2", contentToWrite).then(() => {
+//             console.log("After the file has been written in the 3rd block");
+//         }).catch((error) => {
+//             console.log("Error occure in a deeply nested promis chain");
+//         });
+//     }).catch(error => {
+//         console.log("Error occured in the 2nd promise chain");
+//     });
     
-}).catch((error) => {
-    console.log("This error occured:", error);
-}).finally(() => {
-    console.log("All file writing has been written");
-});
-console.log("After the promise");
+// }).catch((error) => {
+//     console.log("This error occured:", error);
+// }).finally(() => {
+//     console.log("All file writing has been written");
+// });
+// console.log("After the promise");
 
 
 
